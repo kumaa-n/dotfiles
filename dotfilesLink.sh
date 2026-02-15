ea1a873df3be7b7ffb92b9bbefb416d4815a3f43
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-02-16 01:19:33 +0900

    feat: add starship

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@
 ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/.config/git ~/.config/git
 ln -s ~/.dotfiles/.config/wezterm ~/.config/wezterm
+ln -s ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
