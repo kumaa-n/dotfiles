51060e48588c975e8e1a2d30312027d6a250f9a1
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-06-01 03:25:47 +0900

    feat: add zsh config

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-ln -s ~/.dotfiles/.zshrc ~/
+mkdir -p ~/.config
+
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.config/git ~/.config/git
 ln -s ~/.dotfiles/.config/wezterm ~/.config/wezterm
 ln -s ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/zsh ~/.config/zsh
