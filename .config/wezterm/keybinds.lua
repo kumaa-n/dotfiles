685dc7656f206bdd16397b788d0a2a4d3ca8a1fa
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-06-07 21:22:02 +0900

    feat: add keybinding to quit Wezterm

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -55,11 +55,13 @@ return {
 		{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
 		-- Tab入れ替え
 		{ key = "{", mods = "LEADER", action = act({ MoveTabRelative = -1 }) },
+		{ key = "}", mods = "LEADER", action = act({ MoveTabRelative = 1 }) },
 		-- Tab新規作成
 		{ key = "t", mods = "SUPER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
 		-- Tabを閉じる
 		{ key = "w", mods = "SUPER", action = act({ CloseCurrentTab = { confirm = true } }) },
-		{ key = "}", mods = "LEADER", action = act({ MoveTabRelative = 1 }) },
+		-- WezTermを終了
+		{ key = "q", mods = "SUPER", action = act.QuitApplication },
 
 		-- 画面フルスクリーン切り替え
 		{ key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
