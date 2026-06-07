4ce6b30bf99a694c47b72571639b9d694f5585bc
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-06-07 17:01:50 +0900

    fix: update wezterm configuration

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,6 +12,7 @@ config.window_background_image_hsb = {
 	saturation = 1.0,
 	hue = 1.0,
 }
+config.color_scheme = "Kanagawa (Gogh)"
 
 -- 合字を無効化
 config.harfbuzz_features = { "calt=0", "clig=0", "dlig=0", "liga=0" }
@@ -19,57 +20,22 @@ config.harfbuzz_features = { "calt=0", "clig=0", "dlig=0", "liga=0" }
 ----------------------------------------------------
 -- Tab
 ----------------------------------------------------
--- タイトルバーを非表示
-config.window_decorations = "RESIZE"
--- タブバーの表示
-config.show_tabs_in_tab_bar = true
--- タブが一つの時は非表示
-config.hide_tab_bar_if_only_one_tab = true
--- falseにするとタブバーの透過が効かなくなる
--- config.use_fancy_tab_bar = false
-
--- タブバーの透過
-config.window_frame = {
-	inactive_titlebar_bg = "none",
-	active_titlebar_bg = "none",
-}
-
--- タブバーを背景色に合わせる
--- config.window_background_gradient = {
--- 	colors = { "#000000" },
--- }
-
--- タブの追加ボタンを非表示
-config.show_new_tab_button_in_tab_bar = false
--- nightlyのみ使用可能
--- タブの閉じるボタンを非表示
-config.show_close_tab_button_in_tabs = false
-
-config.color_scheme = "Kanagawa (Gogh)"
-
--- タブ同士の境界線を非表示
-config.colors = {
-	tab_bar = {
-		inactive_tab_edge = "none",
-	},
-}
-
--- タブの形をカスタマイズ
--- タブの左側の装飾
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
--- タブの右側の装飾
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local background = "#5c6d74"
+	local background = "#658594"
 	local foreground = "#FFFFFF"
 	local edge_background = "none"
+
 	if tab.is_active then
-		background = "#ae8b2d"
+		background = "#938056"
 		foreground = "#FFFFFF"
 	end
+
 	local edge_foreground = background
-	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	local index = tab.tab_index + 1
+	local title = "   " .. index .. ": " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
 	return {
 		{ Background = { Color = edge_background } },
 		{ Foreground = { Color = edge_foreground } },
