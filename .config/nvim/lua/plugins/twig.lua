0e60d6a4a207bc1bddbc154d9f221cbb788673e1
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-06-16 23:57:52 +0900

    feat: add twig formatters

--- .config/nvim/lua/plugins/twig.lua
+++ .config/nvim/lua/plugins/twig.lua
@@ -31,7 +31,22 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
         "twiggy-language-server",
+        "twig-cs-fixer",
       })
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.twig = { "twig-cs-fixer", "djhtml" }
+      opts.formatters = opts.formatters or {}
+      opts.formatters.djhtml = {
+        command = "djhtml",
+        args = { "-" },
+        stdin = true,
+      }
     end,
   },
 
