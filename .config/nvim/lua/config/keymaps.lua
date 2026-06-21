05932a4bd42855560e9ba5a5bb3776a90434e1c1
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-06-21 23:23:19 +0900

    feat: add yank keymaps for path and branch

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,25 @@ vim.keymap.set("n", "<C-S-h>", "<C-w><", { desc = "Decrease window width" })
 vim.keymap.set("n", "<C-S-l>", "<C-w>>", { desc = "Increase window width" })
 vim.keymap.set("n", "<C-S-j>", "<C-w>+", { desc = "Increase window height" })
 vim.keymap.set("n", "<C-S-k>", "<C-w>-", { desc = "Decrease window height" })
+
+-- Yank git-relative path
+vim.keymap.set("n", "<leader>yg", function()
+  local prefix = vim.fn.system("git rev-parse --show-prefix"):gsub("\n", "")
+  local path = prefix .. vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  print("Copied git-relative path: " .. path)
+end, { desc = "Yank git-relative path" })
+
+-- Yank full path
+vim.keymap.set("n", "<leader>yf", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print("Copied full path: " .. path)
+end, { desc = "Yank full path" })
+
+-- Yank current branch name
+vim.keymap.set("n", "<leader>yb", function()
+  local branch = vim.fn.system("git branch --show-current"):gsub("\n", "")
+  vim.fn.setreg("+", branch)
+  print("Copied branch: " .. branch)
+end, { desc = "Yank branch name" })
