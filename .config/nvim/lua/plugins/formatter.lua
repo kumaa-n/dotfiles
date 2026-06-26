9301a9aa267a7e8776c63b2be62cb987a79bc1c8
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-06-26 15:12:15 +0900

    feat: add support to deno

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -18,4 +18,24 @@ return {
       },
     },
   },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = { "deno_fmt", "prettier", stop_after_first = true },
+        typescriptreact = { "deno_fmt", "prettier", stop_after_first = true },
+        javascript = { "deno_fmt", "prettier", stop_after_first = true },
+        javascriptreact = { "deno_fmt", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        deno_fmt = {
+          -- deno.json(c) があるプロジェクトでのみ実行する
+          condition = function(_, ctx)
+            return require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")(ctx.filename) ~= nil
+          end,
+        },
+      },
+    },
+  },
 }
--- .config/nvim/lua/plugins/javascript.lua
+++ .config/nvim/lua/plugins/javascript.lua
@@ -3,17 +3,55 @@ return {
     "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "typescript-language-server" })
+      vim.list_extend(opts.ensure_installed, { "vtsls" })
     end,
   },
 
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ts_ls = {},
-      },
-    },
+    opts = function(_, opts)
+      local root_pattern = require("lspconfig.util").root_pattern
+
+      opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+        -- deno.json(c) があるプロジェクトでは起動しない
+        vtsls = {
+          mason = false,
+          root_dir = function(bufnr, on_dir)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            local deno_filepath = root_pattern("deno.json", "deno.jsonc")(fname)
+            if deno_filepath ~= nil then
+              return
+            end
+            on_dir(root_pattern("tsconfig.json", "package.json")(fname))
+          end,
+        },
+        -- deno.json(c) があるプロジェクトでのみ起動
+        denols = {
+          mason = false,
+          root_dir = function(bufnr, on_dir)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            local filepath = root_pattern("deno.json", "deno.jsonc")(fname)
+            if filepath == nil then
+              return
+            end
+            on_dir(filepath)
+          end,
+          handlers = {
+            -- exports フィールドのない npm パッケージのサブパス import エラーを除外する。
+            -- denols は pull 型診断 (textDocument/diagnostic) を使うため、
+            -- push 型 (textDocument/publishDiagnostics) ではなくこちらを上書きする必要がある。
+            ["textDocument/diagnostic"] = function(err, result, ctx, config)
+              if result and result.items then
+                result.items = vim.tbl_filter(function(d)
+                  return not (d.message and d.message:find("does not define an export", 1, true))
+                end, result.items)
+              end
+              vim.lsp.diagnostic.on_diagnostic(err, result, ctx, config)
+            end,
+          },
+        },
+      })
+    end,
   },
 
   {
