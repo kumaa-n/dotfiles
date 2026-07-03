f28961ffa5ff143472bafcd8d80dc09a5c39823d
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-03 10:54:08 +0900

    feat: add keymap description for h

--- .config/nvim/lua/plugins/html-preview.lua
+++ .config/nvim/lua/plugins/html-preview.lua
@@ -4,6 +4,7 @@ return {
     cmd = { "LivePreview" },
     opts = {},
     keys = {
+      { "<leader>h", "", desc = "HTML" },
       { "<leader>hp", "<cmd>LivePreview start<cr>", desc = "Start HTML Live Preview" },
       { "<leader>hP", "<cmd>LivePreview close<cr>", desc = "Stop HTML Live Preview" },
       { "<leader>hf", "<cmd>LivePreview pick<cr>", desc = "Pick File to Preview" },
