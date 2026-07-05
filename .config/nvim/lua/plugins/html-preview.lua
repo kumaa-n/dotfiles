63f4a4bb4c55557c1004c7b6bde2b98c9e6f8d55
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-05 23:36:00 +0900

    fix: register <leader>h/<leader>t which-key groups in visual mode

--- .config/nvim/lua/plugins/html-preview.lua
+++ .config/nvim/lua/plugins/html-preview.lua
@@ -4,7 +4,7 @@ return {
     cmd = { "LivePreview" },
     opts = {},
     keys = {
-      { "<leader>h", "", desc = "HTML" },
+      { "<leader>h", "", mode = { "n", "v" }, desc = "HTML" },
       { "<leader>hp", "<cmd>LivePreview start<cr>", desc = "Start HTML Live Preview" },
       { "<leader>hP", "<cmd>LivePreview close<cr>", desc = "Stop HTML Live Preview" },
       { "<leader>hf", "<cmd>LivePreview pick<cr>", desc = "Pick File to Preview" },
