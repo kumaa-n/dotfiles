3651627d610a7a0d2d0fa9ea72de202a14df1bf6
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-06 23:08:49 +0900

    feat: add zsh-abbr and migrate docker/bundle aliases to abbreviations

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -22,17 +22,6 @@ alias rm='rm -i'
 alias ld='lazydocker'
 alias lg='lazygit'
 
-alias dc='show_command docker compose'
-alias dcb='show_command docker compose build'
-alias dcd='show_command docker compose down'
-alias dce='show_command docker compose exec'
-alias dcr='show_command docker compose run'
-alias dcu='show_command docker compose up'
-alias dstopall='show_command docker container stop $(docker container ls -aq)'
-
-alias be='bundle exec'
-alias bi='bundle install'
-
 alias -g C='| pbcopy'
 alias -g F='| fzf'
 alias -g G='| grep'
--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -18,6 +18,16 @@ antidote_dir=/usr/local/share/antidote
 source "$antidote_dir/antidote.zsh"
 antidote load "${ZDOTDIR:-$HOME/.config/zsh}/.zsh_plugins.txt"
 
+# zsh-abbr
+abbr -S -qq dcb='docker compose build'
+abbr -S -qq dcd='docker compose down'
+abbr -S -qq dce='docker compose exec'
+abbr -S -qq dcr='docker compose run'
+abbr -S -qq dcu='docker compose up'
+abbr -S -qq dstopall='docker container stop $(docker container ls -aq)'
+abbr -S -qq be='bundle exec'
+abbr -S -qq bi='bundle install'
+
 # starship/zsh-autosuggestions 等が zle-keymap-select をラップすることで
 # FUNCNEST エラーが起きるため、全プラグイン読み込み後に上書きする
 # bindkey -v (viモード) のキーマップ切り替え時にプロンプトを再描画する
