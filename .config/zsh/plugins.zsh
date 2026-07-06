0d189c64990ecb5cc72f18e8481abe4815a50a67
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-06 22:37:41 +0900

    refactor: switch zsh plugin manager from zinit to antidote

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -11,36 +11,12 @@ if [[ $- == *i* ]] && command_exists zoxide; then
   alias cdi='ji'
 fi
 
-# zsh-autocomplete
-# source ~/.zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-
-# zsh-autosuggestions
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-### Added by Zinit's installer
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-        print -P "%F{33} %F{34}Installation successful.%f%b" || \
-        print -P "%F{160} The clone has failed.%f%b"
-fi
-
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
-
-# Load a few important annexes, without Turbo
-# (this is currently required for annexes)
-zinit light-mode for \
-    zdharma-continuum/zinit-annex-as-monitor \
-    zdharma-continuum/zinit-annex-bin-gem-node \
-    zdharma-continuum/zinit-annex-patch-dl \
-    zdharma-continuum/zinit-annex-rust
-
-### End of Zinit's installer chunk
-
-# zinit light zsh-users/zsh-syntax-highlighting
+# antidote
+# 管理するプラグインは .zsh_plugins.txt に記述する
+antidote_dir=/usr/local/share/antidote
+[[ -d $antidote_dir ]] || antidote_dir=/opt/homebrew/share/antidote
+source "$antidote_dir/antidote.zsh"
+antidote load "${ZDOTDIR:-$HOME/.config/zsh}/.zsh_plugins.txt"
 
 # starship/zsh-autosuggestions 等が zle-keymap-select をラップすることで
 # FUNCNEST エラーが起きるため、全プラグイン読み込み後に上書きする
