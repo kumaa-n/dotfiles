ca993ebd467e2d800bbc7a93b4ef9d774f4d3b1a
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-08 11:52:38 +0900

    feat: add history option

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -5,9 +5,15 @@ export PS1='%n@%1~ %#'
 export PATH="$HOME/.local/bin:$PATH"
 
 # history
+HISTFILE="$HOME/.zsh_history"
 HISTSIZE=100000
 SAVEHIST=100000
 
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt SHARE_HISTORY
+
+# vim-mode
 bindkey -v
 
 # History Beginning Search
