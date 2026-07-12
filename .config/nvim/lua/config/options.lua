2daee292a19f683bf9aa712ca4ee43c757fab29b
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-12 15:45:03 +0900

    chore: disable conceallevel to keep markdown markup visible

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.conceallevel = 0 -- LazyVim の 2 を無効化（編集中に ``` 等が隠れるのを防ぐ）
