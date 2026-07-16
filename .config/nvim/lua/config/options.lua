27f16277461a30a17d74674cbb5e82fc7d1a6e99
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-16 22:30:28 +0900

    feat: visualize tabs and whitespace with listchars

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,13 @@
 -- Add any additional options here
 
 vim.opt.conceallevel = 0 -- LazyVim の 2 を無効化（編集中に ``` 等が隠れるのを防ぐ）
+
+-- タブとスペースを視覚化
+vim.opt.listchars = {
+  tab = "▸ ",
+  lead = "·",
+  trail = "·",
+  nbsp = "␣",
+  extends = "❯",
+  precedes = "❮",
+}
