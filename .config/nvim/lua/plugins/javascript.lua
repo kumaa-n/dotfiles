aef9238fac78f280a33a001dc36781be2a8dab4b
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-21 10:59:09 +0900

    fix: guard root_dir callbacks against nil directories

--- .config/nvim/lua/plugins/javascript.lua
+++ .config/nvim/lua/plugins/javascript.lua
@@ -13,33 +13,29 @@ return {
       local root_pattern = require("lspconfig.util").root_pattern
 
       opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
-        -- deno.json(c) があるプロジェクトでは起動しない
         vtsls = {
-          mason = false,
           root_dir = function(bufnr, on_dir)
             local fname = vim.api.nvim_buf_get_name(bufnr)
-            local deno_filepath = root_pattern("deno.json", "deno.jsonc")(fname)
-            if deno_filepath ~= nil then
+            if root_pattern("deno.json", "deno.jsonc")(fname) ~= nil then
               return
             end
-            on_dir(root_pattern("tsconfig.json", "package.json")(fname))
+            local dir = root_pattern("tsconfig.json", "package.json")(fname)
+            if dir then
+              on_dir(dir)
+            end
           end,
         },
-        -- deno.json(c) があるプロジェクトでのみ起動
         denols = {
           mason = false,
           root_dir = function(bufnr, on_dir)
             local fname = vim.api.nvim_buf_get_name(bufnr)
-            local filepath = root_pattern("deno.json", "deno.jsonc")(fname)
-            if filepath == nil then
+            local dir = root_pattern("deno.json", "deno.jsonc")(fname)
+            if dir == nil then
               return
             end
-            on_dir(filepath)
+            on_dir(dir)
           end,
           handlers = {
-            -- exports フィールドのない npm パッケージのサブパス import エラーを除外する。
-            -- denols は pull 型診断 (textDocument/diagnostic) を使うため、
-            -- push 型 (textDocument/publishDiagnostics) ではなくこちらを上書きする必要がある。
             ["textDocument/diagnostic"] = function(err, result, ctx, config)
               if result and result.items then
                 result.items = vim.tbl_filter(function(d)
@@ -57,6 +53,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
         "javascript",
         "typescript",
