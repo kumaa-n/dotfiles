b19bea2a1daa5511ec0e3ac565747333906fe09c
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-07-22 10:11:22 +0900

    feat: manage yazi config via dotfiles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -24,4 +24,5 @@ link ~/.dotfiles/.config/wezterm ~/.config/wezterm
 link ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
 link ~/.dotfiles/.config/nvim ~/.config/nvim
 link ~/.dotfiles/.config/zsh ~/.config/zsh
+link ~/.dotfiles/.config/yazi ~/.config/yazi
 link ~/.dotfiles/.zsh/completion ~/.zsh/completion
