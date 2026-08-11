405e5d5f91f3404d1684c0f992d2ae1963a9326e
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-08-12 08:47:28 +0900

    feat: add prisma plugins for neovim

---
+++ .config/nvim/lua/plugins/prisma.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "prisma-language-server" })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        prismals = {},
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "prisma" })
+    end,
+  },
+}
