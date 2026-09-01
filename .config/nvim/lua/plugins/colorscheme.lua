eacc8bf27289b1ad99ec6aba376c56e96e86790a
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-09-01 16:46:33 +0900

    chore: add colorscheme candidates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,9 @@
 return {
   {
     "rebelot/kanagawa.nvim",
+    -- "neanias/everforest-nvim",
+    -- "folke/tokyonight.nvim",
+    -- "craftzdog/solarized-osaka.nvim",
     lazy = true,
     priority = 1000,
   },
@@ -9,6 +12,9 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa",
+      -- colorscheme = "everforest",
+      -- colorscheme = "tokyonight",
+      -- colorscheme = "solarized-osaka",
     },
   },
 }
