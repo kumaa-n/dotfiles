6aa86d04ac7713b359ed2f0e6e5f64eacb2fc9a7
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-09-02 15:59:24 +0900

    refactor: split color and tab settings into separate modules

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,62 +9,13 @@ config.window_background_image_hsb = {
 	brightness = 0.2,
 }
 
-config.color_scheme = "Kanagawa (Gogh)"
-local palette = {
-	sumiInk0 = "#16161D",
-	sumiInk4 = "#2A2A37",
-	fujiGray = "#727169",
-	crystalBlue = "#7E9CD8",
-}
-
 -- 合字を無効化
 config.harfbuzz_features = { "calt=0", "clig=0", "dlig=0", "liga=0" }
 
-----------------------------------------------------
--- Tab
-----------------------------------------------------
-config.window_decorations = "RESIZE"
-config.show_tabs_in_tab_bar = true
-config.use_fancy_tab_bar = false
-config.show_new_tab_button_in_tab_bar = false
+require("colors").apply(config)
+require("tabs").apply(config)
 
-local colors = {
-	inactive_bg = palette.sumiInk0,
-	inactive_fg = palette.fujiGray,
-	active_bg = palette.sumiInk4,
-	active_fg = palette.crystalBlue,
-}
-
-config.colors = {
-	tab_bar = {
-		background = colors.inactive_bg,
-	},
-}
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local background = colors.inactive_bg
-	local foreground = colors.inactive_fg
-	local underline = "None"
-
-	if tab.is_active then
-		background = colors.active_bg
-		foreground = colors.active_fg
-		underline = "Single"
-	end
-
-	local index = tab.tab_index + 1
-	local title = "   " .. index .. ": " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-	return {
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Attribute = { Underline = underline } },
-		{ Text = title },
-	}
-end)
-
-----------------------------------------------------
 -- keybinds
-----------------------------------------------------
 config.disable_default_key_bindings = true
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
