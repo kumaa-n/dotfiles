3bc93c0ad7e0cfda7a5068ee73d777411c6f2e12
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-09-02 16:16:04 +0900

    feat: hide tab bar when only one tab is open

--- .config/wezterm/tabs.lua
+++ .config/wezterm/tabs.lua
@@ -33,6 +33,7 @@ return {
 	apply = function(config)
 		config.window_decorations = "RESIZE"
 		config.show_tabs_in_tab_bar = true
+		config.hide_tab_bar_if_only_one_tab = true
 		config.use_fancy_tab_bar = false
 		config.show_new_tab_button_in_tab_bar = false
 
